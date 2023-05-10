cc5175836fec144879bec87abe51f1c2ede6b572
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-05-10 22:49:47 +0200

    first test

---
+++ javascript.js
@@ -0,0 +1,31 @@
+/* PSEUDOCODE:
+
+When a user inputs a number
+Loop from 1 to the entered number
+If the current number is divisible by 3 then print "Fizz"
+If the current number is divisible by 5 then print "Buzz"
+If the current number is divisible by 3 and 5 then print "FizzBuzz"
+Otherwise print the current number
+
+*/
+
+/* 
+
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+for (let i = 1; i <= answer; i++) {
+  if (i % 3 === 0) console.log("Fizz");
+
+  if (i % 5 ===0) console.log("Buzz");
+  
+    else console.log(i);
+
+}
+
+*/
+
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+for (let i = 1; i <= answer; i++) {
+  console.log(i);
+}
